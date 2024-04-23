9986604f6afc1b9b2a59cf271ba6c3dd49232a88
HJ5323 <167291731+HJ5323@users.noreply.github.com>
2024-04-23 18:03:57 +0900

    1부터의 숫자합

---
+++ 0423_prac/prac_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// 1부터 사용자가 입력한 숫자까지의 합을 출력하는 코드 작성
+int main()
+{
+	int input, output = 0;
+
+	std::cout << "1부터 n까지의 합 구하기" << std::endl;
+	std::cout << "숫자(양의 정수)를 입력하세요. : " ;
+	std::cin >> input;
+
+	for (int i = 1; i <= input; i++) // 1부터 9까지 반복하면서 1씩 증가
+	{
+		
+		output += i;
+
+	}
+	std::cout << "1부터 " << input << "까지의 합은 :" << output << std::endl;
+
+	return 0;
+
+}
